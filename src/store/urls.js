8e950d0f92589400d1dd498413352ebfdca335b3
EliteGams <20193531+EliteGams@users.noreply.github.com>
2020-08-17 15:58:49 -0700

    Update urls.js

--- src/store/urls.js
+++ src/store/urls.js
@@ -1,9 +1,9 @@
-const BASE_URL = "https://gogoanime.vercel.app/api/v1";
+const BASE_URL = "https://animefreak.vercel.app/";
 
-export const LATEST = `${BASE_URL}/LatestEpisodes/1`;
+export const LATEST = `${BASE_URL}/LatestEpisodes/`;
 export const LETTER = `${BASE_URL}/Alphabet`;
 export const SEARCH = `${BASE_URL}/Search`;
 export const GENRES = `${BASE_URL}/Genders`;
 export const MOVIES = `${BASE_URL}/Movies`;
-export const OVA = `${BASE_URL}/ovas`;
-export const VIDEO = `${BASE_URL}/DecodeVidstreamingIframeURL`;
+export const OVA = `${BASE_URL}/Ovas`;
+export const VIDEO = `${BASE_URL}/anime`;
