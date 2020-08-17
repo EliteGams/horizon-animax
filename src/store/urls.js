36c1e10781d28c0ab1ed2b709267f7addc876c0a
EliteGams <20193531+EliteGams@users.noreply.github.com>
2020-08-17 15:52:22 -0700

    test

--- src/store/urls.js
+++ src/store/urls.js
@@ -1,9 +1,9 @@
-const BASE_URL = "https://ryuanime-rho.vercel.app/api/v1";
+const BASE_URL = "https://gogoanime.vercel.app/api/v1";
 
-export const LATEST = `${BASE_URL}/latest`;
-export const LETTER = `${BASE_URL}/letter`;
-export const SEARCH = `${BASE_URL}/search`;
-export const GENRES = `${BASE_URL}/genders`;
-export const MOVIES = `${BASE_URL}/movies`;
+export const LATEST = `${BASE_URL}/LatestEpisodes/1`;
+export const LETTER = `${BASE_URL}/Alphabet`;
+export const SEARCH = `${BASE_URL}/Search`;
+export const GENRES = `${BASE_URL}/Genders`;
+export const MOVIES = `${BASE_URL}/Movies`;
 export const OVA = `${BASE_URL}/ovas`;
-export const VIDEO = `${BASE_URL}/anime`;
+export const VIDEO = `${BASE_URL}/DecodeVidstreamingIframeURL`;
