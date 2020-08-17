1083513ed7e15ba9c807bd20fcb31afa82e99ddb
EliteGams <20193531+EliteGams@users.noreply.github.com>
2020-08-17 15:38:36 -0700

    Update urls.js

--- src/store/urls.js
+++ src/store/urls.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://ryuanime-api.chrismichael.now.sh/api/v1";
+const BASE_URL = "https://ryuanime-rho.vercel.app/api/v1";
 
 export const LATEST = `${BASE_URL}/latest`;
 export const LETTER = `${BASE_URL}/letter`;
