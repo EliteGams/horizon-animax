a5b59b7a82236a45a46f53fd4a39a2862bb33979
EliteGams <20193531+EliteGams@users.noreply.github.com>
2020-08-17 16:30:46 -0700

    Update urls.js

--- src/store/urls.js
+++ src/store/urls.js
@@ -1,9 +1,9 @@
-const BASE_URL = "https://animefreak.vercel.app/api/v1";
+const BASE_URL = "https://ryuanime-api.chrismichael.now.sh/api/v1";
 
-export const LATEST = `${BASE_URL}/LatestEpisodes/`;
-export const LETTER = `${BASE_URL}/Alphabet`;
-export const SEARCH = `${BASE_URL}/Search`;
-export const GENRES = `${BASE_URL}/Genders`;
-export const MOVIES = `${BASE_URL}/Movies`;
-export const OVA = `${BASE_URL}/Ovas`;
+export const LATEST = `${BASE_URL}/latest`;
+export const LETTER = `${BASE_URL}/letter`;
+export const SEARCH = `${BASE_URL}/search`;
+export const GENRES = `${BASE_URL}/genders`;
+export const MOVIES = `${BASE_URL}/movies`;
+export const OVA = `${BASE_URL}/ovas`;
 export const VIDEO = `${BASE_URL}/anime`;
